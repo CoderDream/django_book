cfa0b3464dea8973bdfdf2b76db421701747b2d3
CoderDream <coderdream@gmail.com>
2014-08-25 13:31:42 +0800

    add current_datetime

--- django_book/urls.py
+++ django_book/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+#include, 
 
-from django_book.views import *
+#from django_book.views import *
+from django_book.views import hello, current_datetime
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,5 +19,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^hello/$', hello),  
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),  
 )
--- django_book/views.py
+++ django_book/views.py
@@ -1,4 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("Hello World!")
+
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>现在时间是 %s.</body></html>" % now
+    return HttpResponse(html)
